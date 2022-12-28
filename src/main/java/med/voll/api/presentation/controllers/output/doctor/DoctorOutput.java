63e9431d165f516a80145a5faf769e6f68673448
Felipe José <lipej@pm.me>
2022-12-28 13:53:11 -0300

    fix: add missing id field

--- src/main/java/med/voll/api/presentation/controllers/output/doctor/DoctorOutput.java
+++ src/main/java/med/voll/api/presentation/controllers/output/doctor/DoctorOutput.java
@@ -2,8 +2,9 @@ package med.voll.api.presentation.controllers.output.doctor;
 
 import med.voll.api.domain.entities.Doctor;
 
-public record DoctorOutput(String name, String email, String crm, String specialty) {
+public record DoctorOutput(String id, String name, String email, String crm, String specialty) {
     public DoctorOutput(Doctor doctor) {
-        this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty().toString());
+        this(doctor.getId().toString(), doctor.getName(), doctor.getEmail(), doctor.getCrm(),
+                doctor.getSpecialty().toString());
     }
 }
